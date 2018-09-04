9558d2bc29b4ef68ba4cb35214efc837908cf71d
John McLoughlin <JohnMc@BatfishSolutions.com>
2018-09-04 13:55:48 +0100

    Initial commit for investigation into Unity & pusher integration

---
+++ Assets/ClockAnimator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public class ClockAnimator : MonoBehaviour
+{
+    // Based on https://unity3d.com/learn/tutorials/topics/scripting/simple-clock?playlist=17117
+    // .NEt 4.6 targeted by https://docs.unity3d.com/Manual/ScriptingRuntimeUpgrade.html
+
+    private const float hoursToDegrees = 360f / 12f;
+    private const float minutesToDegrees = 360f / 60f;
+    private const float secondsToDegrees = 360f / 60f;
+
+    public Transform hours;
+    public Transform minutes;
+    public Transform seconds;
+
+    public bool analogue;
+    private PusherManager _pusherManager;
+
+    void Start()
+    {
+        if (_pusherManager == null)
+        {
+            _pusherManager = GetComponent<PusherManager>();
+        }
+    }
+
+    private void Update()
+    {
+        var time = DateTime.Now;
+
+        if (analogue)
+        {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees);
+        }
+        else
+        {
+            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        }
+
+        if (time.Second == 0 ||
+            time.Second == 15 ||
+            time.Second == 30 ||
+            time.Second == 45)
+        {
+            _pusherManager.Message($"The time sponsored by Pusher is {time.ToLongTimeString()}");
+        }
+    }
+
+
+}
---
+++ Assets/PusherManager.cs
@@ -0,0 +1,73 @@
+﻿using PusherClient;
+using UnityEngine;
+
+public class PusherManager : MonoBehaviour
+{
+    // A mutation of https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/writing-game-manager
+
+    public static PusherManager PusherManagerInstance = null;
+
+    private Pusher _pusher;
+    private Channel _channel;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        if (PusherManagerInstance == null)
+        {
+            PusherManagerInstance = this;
+            InitialisePusher();
+        }
+        else if (PusherManagerInstance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void InitialisePusher()
+    {
+        if (_pusher == null)
+        {
+            _pusher = new Pusher("", new PusherOptions
+            {
+                // We'll use the autorised from the Client solution
+                Authorizer = new HttpAuthorizer("http://localhost:8888/auth/Unity")
+            });
+
+            _pusher.Error += (s, e) =>
+            {
+                var i = 1;
+            };
+            _pusher.Connect();
+            _channel = _pusher.Subscribe("TestChannel");
+            _channel.Subscribed += (s) =>
+            {
+                var j = 2;
+            };
+        }
+    }
+
+    public void Message(string message)
+    {
+        var k = 1;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Click"))
+        {
+            //if (OnClicked != null)
+            //    OnClicked();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_pusher != null)
+        {
+            _pusher.Disconnect();
+        }
+    }
+}
